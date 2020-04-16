6433dd13135fc06059f4c49def15f6c5dbfbb50a
Usama <Usama18@icloud.com>
2020-04-16 19:12:48 +0500

    integrated theme and introduced a laws controller

---
+++ LawManagementSystem/Controllers/LawsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LawManagementSystem.Controllers
+{
+    public class LawsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
