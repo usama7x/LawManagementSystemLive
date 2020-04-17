e2f258abe9055f7949e6565f33734d57748cf308
Usama <Usama18@icloud.com>
2020-04-17 19:18:53 +0500

    implemented search on laws page

--- LawManagementSystem/Controllers/LawsController.cs
+++ LawManagementSystem/Controllers/LawsController.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LawManagementSystem.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LawManagementSystem.Controllers
 {
     public class LawsController : Controller
     {
-        public IActionResult Index()
+        readonly AppDbContext dbContext;
+        public LawsController(AppDbContext dbContext)
         {
-            return View();
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index(string searchString)
+        {
+
+            var laws = dbContext.Laws.ToList();
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var result = laws.Where(x => x.SectionNo.Contains(searchString) || x.Description
+                .Contains(searchString)).ToList();
+                return View(result);
+            }
+            return View(laws);
         }
     }
 }
